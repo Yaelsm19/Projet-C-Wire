1229299af62ef85f066047ea40e63c89bb9eccbf
Yaelsm19 <yaelsm19@gmail.com>
2024-12-12 20:25:44 +0000

    finalisation verification arguments

--- c-wire.sh
+++ c-wire.sh
@@ -1,57 +1,68 @@
-
-
-nb_args=$#
 afficher_aide() {
     echo "Voici les différents paramètres que vous pouvez remplir"
     echo "parametre 1 : chemin du fichier d'entrée"
     echo "parametre 2 : type de station : hvb ou hva ou lv"
-    echo "parametre 3 : type de consommateur : comp ou indiv ou all
-    options interdites : hvb all ou hdv indiv ou hva all hva indiv"
-    echo "parametre 4 : Optionelle : identifiant de centrale : filtre les résultats pour une centrale spécifique"
-    echo "parametre 5 : Option d’aide (-h)"
+    echo "parametre 3 : type de consommateur : comp ou indiv ou all"
+    echo "combinaisons interdites : hvb all ou hdv indiv ou hva all hva indiv"
+    echo "parametre 4 : Optionel : identifiant de centrale : 1, 2 ou 3"
 }
-verif_3arguments() {
-    if [ ! -f "$1" ]; then
-        echo "Le fichier spécifié n'existe pas ou n'est pas un fichier valide."
-        return 1
-    elif [[ "$1" != *.dat ]]; then
-        echo "Le fichier spécifié n'est pas un fichier dat."
-        return 1
-    elif [ "$2" != "hvb" ] && [ "$2" != "hva" ] && [ "$2" != "lv" ] ; then
-        echo "Le deuxième paramètre n'est pas bon."
-        return 1
-    elif [ "$3" != "comp" ] && [ "$3" != "indiv" ] && [ "$3" != "all" ]; then
-        echo "Le troisième paramètre n'est pas bon."
-        return 1
-    elif [[ "$2" == "hvb" || "$2" == "hva" ]] && [[ "$3" == "indiv" || "$3" == "all" ]]; then
-        echo "Combinaison de paramètres 2 et 3 impossible."
-        return 1
-    fi
-    return 0
-}
-
-verif_tout_arguments(){
+verification_demande_aide(){
     for param in "$@"; do
         if [ "$param" == "-h" ]; then
             afficher_aide
-            return 1
+            exit 1
         fi
     done
-    if [ $nb_args -lt 3 ] || [ $nb_args -gt 6 ];then
+}
+verif_3arguments() {
+    verification=0
+    if [[ ! -f "$chemin_fichier" ]]; then
+        echo "Le fichier spécifié n'existe pas ou n'est pas un fichier valide."
+        verification=1
+    fi
+    if [[ "$chemin_fichier" != *.dat ]]; then
+        echo "Le fichier spécifié n'est pas un fichier dat."
+        verification=1
+    fi
+    if [[ "$type_station" != "hvb" && "$type_station" != "hva" && "$type_station" != "lv" ]] ; then
+        echo "Le deuxième paramètre n'est pas bon."
+        verification=1
+    fi
+    if [[ "$type_conso" != "comp" && "$type_conso" != "indiv" && "$type_conso" != "all" ]]; then
+        echo "Le troisième paramètre n'est pas bon."
+        verification=1
+    fi
+    if [[ "$type_station" == "hvb" || "$type_station" == "hva" ]] && [[ "$type_conso" == "indiv" || "$type_conso" == "all" ]]; then
+        echo "Combinaison de paramètres 2 et 3 impossible."
+        verification=1
+    fi
+    if [[ "$verification" = 1 ]]; then
+        afficher_aide
+        exit 1
+    fi
+}
+verif_tout_arguments(){
+    if [[ $nb_args -lt 3 || $nb_args -gt 6 ]];then
         echo "Nombre de paramètres incorrect"
         afficher_aide
-        return 1
-    elif [ $nb_args -eq 3 ];then
-        verif_3arguments "$@"
-        return $?
-    elif [ $nb_args -eq 4 ];then
-        if [ $4 -ne 1 ] && [ $4 -ne 2 ] && [ $4 -ne 3 ] && [ $4 -ne 4 ] && [ $4 -ne 5 ]; then
-            return 1
+        exit 1
+    elif [[ $nb_args -eq 3 ]];then
+        verif_3arguments
+    elif [[ $nb_args -eq 4 ]]; then
+    verif_3arguments
+        if [[ "$id_centrale" != 1 && "$id_centrale" != 2 && "$id_centrale" != 3 && "$id_centrale" != 4 && "$id_centrale" != 5 ]]; then
+            echo "Le quatrième paramètre n'est pas bon"
+            exit 1
         fi
     fi
 
-    return 0
 }
-verif_tout_arguments "$@"
+nb_args=$#
+verification_demande_aide "$@"
+chemin_fichier=$1
+type_station=$2
+type_conso=$3
+id_centrale=$4
+verif_tout_arguments
 
 
