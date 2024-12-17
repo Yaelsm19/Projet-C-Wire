86bc592c375a0b68fed7270a89f96e763b48faec
ocnmrl <oceane.morel270605@gmail.com>
2024-12-17 12:02:36 +0000

    ajout des commentaires

--- main.c
+++ main.c
@@ -2,202 +2,203 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct station{
-    int id_station;
-    long capacite;
-    long somme_conso;
-}Station;
+typedef struct station{  // définition de la structure représentant une station
+    int id_station;  // identifiant de la station
+    long capacite;  // capacite de la station
+    long somme_conso;  // somme des consommations associées à cette station
+}Station;  
 
-typedef struct arbre{
-    Station station;
-    struct arbre* droit;
-    struct arbre* gauche;
-    int equilibre;
+typedef struct arbre{  // définition de la structure d'un nœud d'arbre AVL
+    Station station;  // données de la station associée à ce nœud
+    struct arbre* droit;  // pointeur vers le fils droit 
+    struct arbre* gauche;  // pointeur vers le fils gauche
+    int equilibre;  // equilibre arbre
 }Arbre;
 
-typedef Arbre* pArbre;
+typedef Arbre* pArbre;  // pointeur arbre
 
-pArbre creer_arbre(int id, long capa){
-    pArbre nouveau = malloc(sizeof(Arbre));
-    if(nouveau==NULL){
-        printf("Erreur lors de l'allocation mémoire");
-        exit(1);
+pArbre creer_arbre(int id, long capa){  // fonction pour creer un nœud d'arbre AVL
+    pArbre nouveau = malloc(sizeof(Arbre));  // alloue dynamiquement un nouveau nœud d'arbre
+    if(nouveau==NULL){  // vérifie si l'allocation mémoire a échoué
+        printf("Erreur lors de l'allocation mémoire");  // message d'erreur
+        exit(1);  // sortie du programme
     }
-    nouveau->droit =NULL;
-    nouveau->gauche = NULL;
-    nouveau->station.id_station = id;
-    nouveau->station.capacite = capa;
-    nouveau->station.somme_conso = 0;
-    nouveau->equilibre = 0;
-    return nouveau;
+    nouveau->droit =NULL;  // initialisation du fils droit
+    nouveau->gauche = NULL;  // initialisation du fils gauche
+    nouveau->station.id_station = id;  // initialisation de l'identifiant de la station
+    nouveau->station.capacite = capa;  // initialisation de la capacite de la station
+    nouveau->station.somme_conso = 0;  // initialisation de consommation totale de la station à 0
+    nouveau->equilibre = 0;  // initialisation de l'equilibre de la station à 0
+    return nouveau;  // retourne le noeud créé
  }
-int min(int a, int b){
+int min(int a, int b){  // fonction calcul du minimum : renvoie le minimum entre deux entiers
     if(a > b){
         return b;
     }
     return a;
 }
 
-int max(int a, int b){
+int max(int a, int b){  // fonction calcul du maximum : renvoie le maximum entre deux entiers
     if(a > b){
         return a;
     }
     return b;
 }
 
-pArbre rotation_droite(pArbre a){
-    pArbre pivot = a->gauche;
-    a->gauche = pivot->droit;
-    pivot->droit = a;
-    int equi_a = a->equilibre;
-    int equi_p = pivot->equilibre;
-    a->equilibre = equi_a - min(equi_p, 0) + 1;
-    pivot->equilibre = max(max(equi_a+2, equi_a + equi_p +2), equi_p +1);
-    a = pivot;
-    return a;
+pArbre rotation_droite(pArbre a){  // fonction pour faire une rotation à droite
+    pArbre pivot = a->gauche;  // le pivot devient le fils gauche du nœud courant
+    a->gauche = pivot->droit;  // le fils gauche devient le pivot du fils droit
+    pivot->droit = a;  // le pivot du fils droit devient a
+    int equi_a = a->equilibre;  // création d'une variable égale à l'équilibre de a
+    int equi_p = pivot->equilibre;  // création d'une variable égale à l'équilibre du pivot
+    a->equilibre = equi_a - min(equi_p, 0) + 1;  // calcul du nouvel equilibre de a
+    pivot->equilibre = max(max(equi_a+2, equi_a + equi_p +2), equi_p +1);  // met à jour le facteur d'équilibre du pivot
+    a = pivot;  // a  est égal au pivot
+    return a;  // retourner l'arbre 
 }
 
-pArbre rotation_gauche(pArbre a){
-    pArbre pivot = a->droit;
-    a->droit = pivot->gauche;
-    pivot->gauche = a;
-    int equi_a = a->equilibre;
-    int equi_p = pivot->equilibre;
-    a->equilibre = equi_a - max(equi_p, 0) -1;
-    pivot->equilibre = min(min(equi_a-2, equi_a + equi_p -2), equi_p-1);
-    a = pivot;
-    return a;
+pArbre rotation_gauche(pArbre a){  // fonction pour faire une rotation à gauche 
+    pArbre pivot = a->droit;  // le pivot devient le fils droit du nœud courant
+    a->droit = pivot->gauche;  // le fils droit devient le pivot du fils gauche
+    pivot->gauche = a;  // le pivot du fils droit devient a
+    int equi_a = a->equilibre;  // création d'une variable egale à l'equilibre du pivot
+    int equi_p = pivot->equilibre;  // création d'une variable egale à l'equilibre du pivot
+    a->equilibre = equi_a - max(equi_p, 0) -1;  // calcul du nouvel equilibre de a
+    pivot->equilibre = min(min(equi_a-2, equi_a + equi_p -2), equi_p-1);  // met à jour le facteur d'équilibre du pivot
+    a = pivot;  // a est égal au pivot 
+    return a;  // retourner l'arbre
 }
 
-pArbre double_rotation_droit(pArbre a){
-    a->gauche = rotation_gauche(a->gauche);
-    a = rotation_droite(a);
-    return a;
+pArbre double_rotation_droit(pArbre a){  // fonction double rotation droite : effectue une double rotation droite sur un nœud AVL
+    a->gauche = rotation_gauche(a->gauche);  // effectue une rotation gauche sur le fils gauche
+    a = rotation_droite(a);  // rotation droite de a
+    return a;  // retourne l'arbre 
 }
 
-pArbre double_rotation_gauche(pArbre a){
-    a->droit = rotation_droite(a->droit);
-    a = rotation_gauche(a);
-    return a;
+pArbre double_rotation_gauche(pArbre a){  // fonction double rotation gauche : effectue une double rotation gauche sur un nœud AVL
+    a->droit = rotation_droite(a->droit);  // effectue une rotation droite sur le fils droit
+    a = rotation_gauche(a);  // rotation gauche de a
+    return a;  // retourne l'arbre 
 }
 
-pArbre equilibrer_AVL(pArbre a){
-    if(a->equilibre >= 2){
-        if(a->droit->equilibre>=0){
-            return rotation_gauche(a);
+pArbre equilibrer_AVL(pArbre a){  // fonction pour équilibrer un nœud AVL si nécessaire après une insertion
+    if(a->equilibre >= 2){  // condition si l'equilibre de a est supérieur ou égal à 2
+        if(a->droit->equilibre>=0){  // condition si l'equilibre du fils droit de a est supérieur ou égal à 0
+            return rotation_gauche(a);  // rotation gauche de a
         }
-        else{
-            return double_rotation_gauche(a);
-        }
-    }
-    else if(a->equilibre<=-2){
-        if(a->gauche->equilibre<=0){
-            return rotation_droite(a);
-        }
-        else{
-            return double_rotation_droit(a);
+        else{  // si l'equilibre du fils droit de a est négatif 
+            return double_rotation_gauche(a);  // double rotation gauche de a
         }
     }
+    else if(a->equilibre<=-2){  // condition si l'équilibre de a inférieur ou égal à -2
+        if(a->gauche->equilibre<=0){  // condition si l'equilibre du fils gauche de a est inférieur ou égal à 0
+            return rotation_droite(a); // rotation droite de a
+        }
+        else{  // si l'equilibre du fils gauche de a est positif 
+            return double_rotation_droit(a);  // double rotation droite de a
+        }
+    }
     return a;
 }
 
-pArbre insert_AVL(pArbre a, int id, long capa, int* h){
-    if(a==NULL){
-        *h = 1;
-        return creer_arbre(id, capa);
+pArbre insert_AVL(pArbre a, int id, long capa, int* h){  // fonction pour inserer dans un AVL tout en maintenant son équilibre
+    if(a==NULL){  // si l'arbre est vide
+        *h = 1;  // indique que la hauteur a augmenté
+        return creer_arbre(id, capa); // créer un nouveau nœud avec l'id et la capacité
     }
-    else if(id <= a->station.id_station){
-        a->gauche = insert_AVL(a->gauche, id, capa, h);
-        *h = -*h;
+    else if(id <= a->station.id_station){  // insérer à gauche si l'id est inférieur ou égal à celui du nœud courant
+        a->gauche = insert_AVL(a->gauche, id, capa, h);  
+        *h = -*h;  // ajuste la hauteur
     }
-    else if(id >= a->station.id_station){
+    else if(id >= a->station.id_station){  // insérer à droite si l'id est supérieur à celui du nœud courant
         a->droit = insert_AVL(a->droit, id, capa, h);
     }
-    else{
+    else{  // si l'id est déjà présent, ne rien faire
         *h = 0;
         return a;
     }
-    if(*h!=0){
-        a->equilibre = a->equilibre + *h;
-        a = equilibrer_AVL(a);
-        if(a->equilibre==0){
-            *h = 0;
+    if(*h!=0){  // met à jour l'équilibre si la hauteur a changé
+        a->equilibre = a->equilibre + *h;  // ajuste le facteur d'équilibre
+        a = equilibrer_AVL(a);  // réequilibre si nécessaire
+        if(a->equilibre==0){  // si l'equilibre de a est egal à 0
+            *h = 0;  // la hauteur ne change pas
         }
-        else{
-            *h = 1;
-        }
-    }
-    return a;
-}
-
-pArbre ajout_consommation_noeud(pArbre a, long consommation, int id_noeud){
-    if(a == NULL){
-        printf("erreur lors du code");
-        exit(1);
-    }
-    else if(id_noeud < a->station.id_station){
-        a->gauche = ajout_consommation_noeud(a->gauche, consommation, id_noeud);
-    }
-    else if(id_noeud > a->station.id_station){
-        a->droit = ajout_consommation_noeud(a->droit, consommation, id_noeud);
-    }
-    else{
-        a->station.somme_conso = a->station.somme_conso + consommation;
-    }
-    return a;
-}
-
-void ecrire(pArbre a, FILE* fichier2){
-    if(a!=NULL){
-        ecrire(a->gauche, fichier2);
-        fprintf(fichier2, "%d;%ld;%ld\n", a->station.id_station, a->station.capacite, a->station.somme_conso);
-        ecrire(a->droit, fichier2);
-        free(a);
-    }
-}
-
-int verifier_nb_argument(int argc){
-    if (argc != 3){
-        printf("Il n'y a pas le bon nombre d'argument");
-        return 0;
-    }
-    return 1;
-
-}
-
-pArbre recuperer_fichier_tmp(FILE* fichier){
-    int id;
-    long capacite;
-    long consommation;
-    int h = 0;
-    pArbre a = NULL;
-    while(fscanf(fichier, "%d;%ld;%ld", &id, &capacite, &consommation) != EOF){
-        if(consommation == 0){
-            a = insert_AVL(a, id, capacite, &h);
-        }
-        else{
-            a = ajout_consommation_noeud(a, consommation, id);
+        else{  // si l'equilibre de à est différent de 0
+            *h = 1;  // indique que la hauteur a augmenté
         }
     }
-    return a;
+    return a;  // retourner l'arbre 
 }
 
-void traitement_total(FILE* fichier_tmp, FILE* fichier_final){
-    pArbre a = NULL;
-    a = recuperer_fichier_tmp(fichier_tmp);
-    ecrire(a, fichier_final);
+pArbre ajout_consommation_noeud(pArbre a, long consommation, int id_noeud){  // fonction pour ajouteur de la consommation à un noeud dans l'arbre 
+    if(a == NULL){  // si l'arbre est vide
+        printf("erreur lors du code");  // message d'erreur
+        exit(1);  // sortie du programme
+    }
+    else if(id_noeud < a->station.id_station){  // si l'id recherché est inférieur à l'id du nœud courant
+        a->gauche = ajout_consommation_noeud(a->gauche, consommation, id_noeud);  // exploration du fils gauche
+    }
+    else if(id_noeud > a->station.id_station){  // si l'id recherché est supérieur à l'id du nœud courant
+        a->droit = ajout_consommation_noeud(a->droit, consommation, id_noeud);  // exploration du fils droit
+    }
+    else{  // si le noeud est trouvé 
+        a->station.somme_conso = a->station.somme_conso + consommation;  // met à jour la consommation totale de la station
+    }
+    return a;  // retourner l'arbre 
 }
 
-int main(int argc, char* argv[]){
-    if(!verifier_nb_argument(argc)){
-        exit (1);
+void ecrire(pArbre a, FILE* fichier2){  // fonction pour écrire les données d'un arbre dans un fichier
+    if(a!=NULL){  // / vérifie si l'arbre n'est pas vide
+        ecrire(a->gauche, fichier2);  // appel récursif pour parcourir le sous-arbre gauche
+        fprintf(fichier2, "%d;%ld;%ld\n", a->station.id_station, a->station.capacite, a->station.somme_conso);  // écrit les informations du nœud courant dans le fichier (id, capacité, consommation)
+        ecrire(a->droit, fichier2);  // appel récursif pour parcourir le sous-arbre droit
+        free(a);  // libère la mémoire allouée pour le nœud courant
     }
-    FILE* fichier_tmp = fopen(argv [1], "r");
-    FILE* fichier_final = fopen(argv [2], "a+");
-    if((fichier_tmp == NULL) || (fichier_final == NULL)){
-        printf("Problème lors de l'ouverture des fichiers");
+}
+
+int verifier_nb_argument(int argc){  // fonction pour vérifier si le programme reçoit le bon nombre d'arguments
+    if (argc != 3){  // vérifie si le nombre d'arguments est différent de 3 
+        printf("Il n'y a pas le bon nombre d'argument");  // message d'erreur 
+        return 0;  // retourne 0 pour signaler une erreur
     }
-    traitement_total(fichier_tmp, fichier_final);
-    fclose(fichier_final);
-    fclose(fichier_tmp);
+    return 1;  // retourne 1 si le nombre d'arguments est correct
+
+}
+
+pArbre recuperer_fichier_tmp(FILE* fichier){  // fonction pour construire un arbre AVL à partir d'un fichier temporaire
+    int id;  // identifiant de la station
+    long capacite;  // capacité de la station
+    long consommation;  // consommation de la station 
+    int h = 0;  // variable pour gérer la hauteur lors de l'insertion dans l'AVL
+    pArbre a = NULL;  // initialise un arbre vide
+    while(fscanf(fichier, "%d;%ld;%ld", &id, &capacite, &consommation) != EOF){  // lit une ligne du fichier jusqu'à la fin (EOF) au format id;capacité;consommation
+        if(consommation == 0){  // si la consommation est 0
+            a = insert_AVL(a, id, capacite, &h);  // ajoute un nouveau nœud dans l'AVL
+        }
+        else{  // sinon
+            a = ajout_consommation_noeud(a, consommation, id);  // ajoute la consommation à un nœud existant dans l'arbre
+        }
+    }
+    return a;  // retourne l'arbre construit
+}
+
+void traitement_total(FILE* fichier_tmp, FILE* fichier_final){  // fonction pour traiter un fichier temporaire et écrire le résultat dans un fichier final
+    pArbre a = NULL;  // initialise un arbre vide
+    a = recuperer_fichier_tmp(fichier_tmp);  // construit l'arbre à partir du fichier temporaire
+    ecrire(a, fichier_final);  // écrit les données de l'arbre dans le fichier final
+}
+
+int main(int argc, char* argv[]){  // fonction principale du programme
+    if(!verifier_nb_argument(argc)){  // vérifie si le nombre d'arguments est correct
+        exit (1);  // quitte le programme en cas d'erreur
+    }
+    FILE* fichier_tmp = fopen(argv [1], "r");  // ouvre le fichier temporaire en mode lecture
+    FILE* fichier_final = fopen(argv [2], "a+");  // ouvre le fichier final en mode ajout/lecture
+    if((fichier_tmp == NULL) || (fichier_final == NULL)){  // vérifie si les fichiers ont été ouverts correctement
+        printf("Problème lors de l'ouverture des fichiers");  // affiche un message d'erreur en cas d'échec
+        exit (1); // quitte le programme en cas d'erreur
+    }
+    traitement_total(fichier_tmp, fichier_final);  // traite les fichiers (construction et écriture des données)
+    fclose(fichier_final);  // ferme le fichier final
+    fclose(fichier_tmp);  // ferme le fichier temporaire
 }
